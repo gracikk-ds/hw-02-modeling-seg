e01df8cb034c6209578be7c3da60cb6360520c4e
Aleksandr Gordeev <grac20101@gmail.com>
2023-12-23 12:04:56 +0300

    added mask to bboxes

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,7 +1,11 @@
 """Utility module for reading dataframes from a specified path based on a given mode."""
 import os
+from typing import List
 
+import numpy as np
 import pandas as pd
+from numpy.typing import NDArray
+from scipy.ndimage import label
 from sklearn.model_selection import train_test_split
 
 TEST_VAL_PROPORTION: float = 0.5
@@ -51,3 +55,32 @@ def split_and_save_datasets(data_path: str, train_size: float) -> None:
     save_df(train_data, data_path, "train")
     save_df(val_data, data_path, "val")
     save_df(test_data, data_path, "test")
+
+
+def masks_to_bboxes(mask: NDArray[np.uint8]) -> List[List[int]]:
+    """
+    Convert a binary mask with potentially multiple objects to a list of bounding boxes in COCO format.
+
+    Args:
+        mask (NDArray[np.uint8]): A binary mask where objects' pixels are 1 and the background is 0.
+
+    Returns:
+        List[List[int]]: A list of bounding boxes, each in the format [x_min, y_min, width, height].
+    """
+    # Label different components (barcodes)
+    labeled_array, num_features = label(mask)
+
+    bboxes = []
+    for barcode_label in range(1, num_features + 1):
+        # Find the bounding box for each labeled component
+        barcode_mask = labeled_array == barcode_label
+        rows = np.any(barcode_mask, axis=1)
+        cols = np.any(barcode_mask, axis=0)
+        y_min, y_max = np.where(rows)[0][[0, -1]]
+        x_min, x_max = np.where(cols)[0][[0, -1]]
+
+        bbox_width = x_max - x_min + 1
+        bbox_height = y_max - y_min + 1
+
+        bboxes.append([int(x_min), int(y_min), int(bbox_width), int(bbox_height)])
+    return bboxes
